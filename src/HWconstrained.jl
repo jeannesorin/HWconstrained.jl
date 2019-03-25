37d76e569b304ceb3a20a6294ed840a3e8657d28
florian oswald <florian.oswald@gmail.com>
2019-03-25 16:21:56 +0100

    added qu

--- src/HWconstrained.jl
+++ src/HWconstrained.jl
@@ -2,4 +2,117 @@ module HWconstrained
 
 greet() = print("Hello World!")
 
+	using JuMP, NLopt, DataFrames, Ipopt
+	using LinearAlgebra
+
+	export data, table_NLopt, table_JuMP
+
+	function data(a=0.5)
+                            
+                                                                      
+                                          
+                                                  
+               
+               
+                         
+                                                                                                                            
+                                             
+		return Dict("a"=>a,"na"=>na,"nc"=>nc,"ns"=>ns,"nss"=>nss,"e"=>e,"p"=>p,"z"=>z,"pi"=>pi)
+	end
+
+
+	function max_JuMP(a=0.5)
+
+		return Dict("obj"=>objective_value(m),"c"=>value(c),"omegas"=>[value(omega[i]) for i in 1:length(omega)])
+	end
+
+	function table_JuMP()
+		d = DataFrame(a=[0.5;1.0;5.0],c = zeros(3),omega1=zeros(3),omega2=zeros(3),omega3=zeros(3),fval=zeros(3))
+		for i in 1:nrow(d)
+			xx = max_JuMP(d[i,:a])
+			d[i,:c] = xx["c"]
+			d[i,:omega1] = xx["omegas"][1]
+			d[i,:omega2] = xx["omegas"][2]
+			d[i,:omega3] = xx["omegas"][3]
+			d[i,:fval] = xx["obj"]
+		end
+		return d
+	end
+
+                        
+                         
+                  
+                          
+                       
+                                    
+
+	function obj(x::Vector,grad::Vector,data::Dict)
+                            
+          
+                  
+             
+                                   
+                                                                               
+        
+                        
+                               
+                         
+                    
+                    
+                                   
+                         
+                                       
+                                                             
+                                                                                                        
+          
+         
+            
+                              
+                                  
+                                   
+               
+	end
+
+	function constr(x::Vector,grad::Vector,data::Dict)
+                            
+          
+                  
+
+                                             
+                                                      
+
+                        
+                             
+                                              
+        
+                                        
+                                        
+                       
+	end
+
+	function max_NLopt(a=0.5)
+             
+                                    
+                                       
+                                               
+                                           
+                                                     
+                     
+                                                
+	end
+
+	function table_NLopt()
+		d = DataFrame(a=[0.5;1.0;5.0],c = zeros(3),omega1=zeros(3),omega2=zeros(3),omega3=zeros(3),fval=zeros(3))
+		for i in 1:nrow(d)
+			xx = max_NLopt(d[i,:a])
+			for j in 2:ncol(d)-1
+				d[i,j] = xx[2][j-1]
+			end
+			d[i,end] = xx[1]
+		end
+		return d
+	end
+
+
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,52 @@ using HWconstrained
 using Test
 
 @testset "HWconstrained.jl" begin
-    # Write your own tests here.
+	@testset "testing components" begin
+
+		@testset "tests gradient of objective function" begin
+                              
+                                        
+                                          
+                                               
+                                                                
+                                               
+		end
+
+
+		@testset "tests gradient of constraint function" begin
+                              
+                              
+                                          
+                                         
+                                                                   
+                                                
+		end
+	end
+
+	@testset "testing result of both maximization methods" begin
+
+		truth = HWconstrained.DataFrame(a=[0.5;1.0;5.0],
+			                            c = [1.008;1.004;1.0008],
+			                            omega1=[-1.41237;-0.20618;0.758763],
+			                            omega2=[0.801455;0.400728;0.0801455],
+			                            omega3=[1.60291;0.801455;0.160291],
+			                            fval=[-1.20821;-0.732819;-0.013422])
+
+		@testset "checking result of NLopt maximization" begin
+
+			t1 = table_NLopt()
+			for c in names(truth)
+				@test all(maximum.(abs.(t1[c].-truth[c])) .< tol2)
+			end
+		end
+
+
+		@testset "checking result of NLopt maximization" begin
+			t1 = table_JuMP()
+			for c in names(truth)
+				@test all(maximum.(abs.(t1[c].-truth[c])) .< tol2)
+			end
+		end
+	end
+
 end
